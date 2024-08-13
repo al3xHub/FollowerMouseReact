80a6da4f686cec39c7254e9efe93a06bb3c444c0
al3xHub <alejandrobueno92@hotmail.com>
2024-08-13 13:27:26 +0200

    follower-mouse

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import './App.css'
+import { useEffect, useState } from 'react'
+
+function App() {
+
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log(clientX, clientY)
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+      setPosition({ x: 0, y: 0 })
+    }
+  }, [enabled])
+
+  return (
+    <>
+      <div style={{
+        position: 'absolute',
+        top: -20,
+        left: -20,
+        width: 40,
+        height: 40,
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        transform: `translate(${position.x}px, ${position.y}px)`,
+      }
+      }>
+      </div>
+
+      <button onClick={() => setEnabled(!enabled)}>{enabled ? 'Deactivate' : 'Activate'} to follow mouse
+      </button>
+    </>
+  )
+}
+
+export default App
